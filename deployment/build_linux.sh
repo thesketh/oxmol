95a0fe46169d2dfa50054dfc898bb7d140a54ed6
Travis Hesketh <travis.hesketh@strath.ac.uk>
2020-05-29 16:20:09 +0100

    Working deployment scripts for linux (tested on Centos 7)

--- deployment/build_linux.sh
+++ deployment/build_linux.sh
@@ -1,20 +1,27 @@
-if ! rustup -V; then 
-    cd install_rust
-    ./install_rust_linux.sh
-    cd ../
+if ! rustup -V /dev/null 2>&1; then
+    if [ ! -d "${HOME}/.cargo" ]; then
+        cd install_rust
+        ./install_rust_linux.sh
+        cd ../
+    fi
     source "${HOME}/.profile"
 fi
 
-cd conda_envs
-if ! ./setup_conda_linux.sh; then
-    echo "Failed to install conda"
-    cd ../
-    exit 1
+if ! conda -V > /dev/null 2>&1; then
+    if [ ! -d "conda" ]; then
+        cd conda_envs
+        if ! ./setup_conda_linux.sh; then
+            echo "Failed to install conda"
+            cd ../
+            exit 1
+        fi
+        cd ../
+    fi
+    export PATH="conda/bin:${PATH}"
 fi
-cd ../
 
 if ! conda activate base > /dev/null 2>&1; then
-    SHELL=$(basename ${SHELL}
+    SHELL=$(basename "${SHELL}")
     source "${HOME}/.${SHELL}rc"
 fi
 
@@ -22,9 +29,9 @@ for env in python36 python37 python38; do
     conda activate $env
     cd ../
     maturin build -i $(which python3) --release
-    if $(which pypy3); then
+    if which pypy3 > /dev/null; then
         maturin build -i $(which pypy3) --release
     fi
     conda deactivate
-    cd development
+    cd deployment
 done
--- deployment/conda_envs/setup_conda_linux.sh
+++ deployment/conda_envs/setup_conda_linux.sh
@@ -1,26 +1,30 @@
+#!/bin/bash
 if ! conda -V > /dev/null 2>&1; then
-    CONDA="Miniconda3-latest-Linux-x86_64.sh"
-    if [ ! -f ${CONDA} ]; then
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    if [ ! -d ../conda ]; then
+        CONDA="Miniconda3-latest-Linux-x86_64.sh"
+        if [ ! -f ${CONDA} ]; then
+            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        fi
+        chmod +x "./${CONDA}"
+        echo "Installing conda."
+        if ! ./"${CONDA}" -b -p ../conda > /dev/null 2>&1; then
+            echo "Something went wrong with the installation."
+            exit 1
+        fi
+        export PATH="../conda/bin:$PATH"
+        SHELL="$(basename ${SHELL})"
+        conda init ${SHELL}
+    else
+        export PATH="../conda/bin:$PATH"
     fi
-    chmod +x "./${CONDA}"
-    echo "Installing conda."
-    if ! ./"${CONDA}" -b -p ../conda > /dev/null 2>&1; then
-        echo "Something went wrong with the installation."
-        exit 1
-    fi
-
-    export PATH="../conda/bin:$PATH"
-    SHELL="$(basename ${SHELL})"
-    conda init ${SHELL}
 fi
 
 if ! conda activate base > /dev/null 2>&1; then
     SHELL="$(basename ${SHELL})"
-    echo "Couldn't use `conda activate`. Re-sourcing ${HOME}/.${SHELL}rc"
-    source "~/.${SHELL}rc"
-    if ! coda activate base > /dev/null 2>&1; then
-        echo "Can't use `conda activate`."
+    echo "Couldn't use conda activate. Re-sourcing ${HOME}/.${SHELL}rc"
+    source "${HOME}/.${SHELL}rc"
+    if ! conda activate base > /dev/null 2>&1; then
+        echo "Can't use conda activate."
         exit 1
     fi
 fi
@@ -29,7 +33,7 @@ for env in python36 python37 python38; do
     if ! conda activate "${env}" > /dev/null 2>&1; then
         conda env create -f "${env}.yml"
     else
-        echo "`${env}` already created."
+        echo "${env} already created."
     fi
     conda deactivate
 done
